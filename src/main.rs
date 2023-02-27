022a699a11e78ea03c83a6bc24d46ac64ebc91eb
Sharif.T <Shariftarver@gmail.com>
2023-02-26 23:52:31 -0500

    converted functions from main to lib, refactored tested

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
 extern crate actix_web;
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-   
-    HttpResponse::Ok() 
-}
+use edtech_newsletter::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-        
-    })
-    .bind("127.0.0.1:3000")?
-    .run()
-    .await
+    run().await
 }
 
 
